3f1e90d77598ad70d876df93e9162791fc3b53a1
ithuhui <hu545548724@163.com>
2019-04-19 01:22:25 +0800

    chore: add some comments

--- hui-bigdata-spark-common/src/main/java/com/hui/bigdata/common/spark/SparkJob.java
+++ hui-bigdata-spark-common/src/main/java/com/hui/bigdata/common/spark/SparkJob.java
@@ -17,11 +17,29 @@ import java.io.Serializable;
  */
 @Slf4j
 public abstract class SparkJob implements Serializable {
+    /**
+     * Instantiates a new Spark job.
+     */
     protected SparkJob(){};
 
+    /**
+     * 带参数
+     *
+     * @param javaSparkContext the java spark context
+     * @param args             the args
+     * @author HuWeihui
+     * @since hui_project v1
+     */
     public void execute(JavaSparkContext javaSparkContext, String[] args) {
     };
 
+    /**
+     * 不带参数
+     *
+     * @param javaSparkContext the java spark context
+     * @author HuWeihui
+     * @since hui_project v1
+     */
     public void execute(JavaSparkContext javaSparkContext) {
     };
 }
